b379b3e9be08f2f5160976e71257431c897fd2b2
alexesca <alexescamore@gmail.com>
2021-07-08 11:20:43 -0600

    Added script to get envelope ids

---
+++ docusign/Q2 with id.py
@@ -0,0 +1,19 @@
+import re
+
+
+ccfile = open("../data/Envelope Report with IDs.csv", "r")
+output = open("../data/contracts.txt", "a")
+i = 0
+list = []
+for aline in ccfile:
+    regex = re.search("(Invoice|Change Order)", aline)
+    if regex == None:
+        if i != 0:
+            id = aline.split(",")[8]
+            list.append(id)
+            output.write(aline)
+        i += 1;
+ccfile.close()
+output.close()
+
+print(list)
