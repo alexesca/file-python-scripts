8435f033e4efabe119fdc8be4be9a6bb3e7e3ee9
alexesca <alexescamore@gmail.com>
2021-07-08 13:01:26 -0600

    Refactored ids to be stored as an  array

--- docusign/Q2 with id.py
+++ docusign/Q2 with id.py
@@ -5,6 +5,7 @@ ccfile = open("../data/Envelope Report with IDs.csv", "r")
 output = open("../data/sold-contracts-ids.txt", "a")
 i = 0
 list = []
+output.write("[")
 for aline in ccfile:
     regex = re.search("(Invoice|Change Order)", aline)
     if regex == None:
@@ -12,9 +13,10 @@ for aline in ccfile:
             id = aline.split(",")[8]
             id = re.findall("([A-Za-z0-9\-]+)", id)
             list.append(id[0])
-            output.write(id[0])
-            output.write('\n')
+            output.write('\'' + id[0] + '\'')
+            output.write(',\n')
         i += 1;
+output.write("]")
 ccfile.close()
 output.close()
 
